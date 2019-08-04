5e29e3277ef01cfc1a71c3110803ba6416390f8a
Marcos Arthur Brito Lima <marcoslyma10@gmail.com>
2019-08-03 23:17:19 -0300

    Excluir por segurança

--- src/config.js
+++
@@ -1,9 +0,0 @@
-//chave privada para salvar com a senha do usuário
-global.SALT_KEY = 'f054654-sfas6543';
-global.EMAIL_TMPL = '<string>Seja bem vindo {0}</string>';
-
-module.exports = {
-    connectionString: 'mongodb://usr_root:0159root@ds257507.mlab.com:57507/giftsdb',
-    sendgridKey: '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',
-    containerConnectionString: 'TBD'
-}
